a9121b8c5bb1921413add9adb3bdff927d9f13fc
rkeenan <ross.keenan@protonmail.comm>
2023-12-03 15:21:04 -0600

    Added div.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,7 +5,11 @@
 </script>
 
 <Header />
-<h1 class="text-red-500 text-4xl">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<Button buttonText="Click me" logMessage="You clicked" />
+
+<div>
+	<h1 class="text-red-500 text-4xl">Welcome to SvelteKit</h1>
+	<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+	<Button buttonText="Click me" logMessage="You clicked" />
+</div>
+
 <Footer />
