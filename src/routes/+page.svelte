d7ad98771e4d8a19a83f9664319d077c5c939ccc
rkeenan <ross.keenan@protonmail.comm>
2023-12-03 15:15:34 -0600

    Added button to page.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,11 @@
 <script>
 	import Header from '../lib/components/Header.svelte';
 	import Footer from '../lib/components/Footer.svelte';
+	import Button from '../lib/components/Button.svelte';
 </script>
 
 <Header />
 <h1 class="text-red-500 text-4xl">Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<Button buttonText="Click me" logMessage="You clicked" />
 <Footer />
