a242f8bf2efc37841d9e849406d04dfec6401d82
rkeenan <ross.keenan@protonmail.comm>
2023-12-03 15:48:05 -0600

    Created real Products endpoint that interacts with the db.

--- src/routes/api/products/+server.js
+++ src/routes/api/products/+server.js
@@ -1,11 +1,33 @@
-export const GET = async () => {
-	const data = {
-		title: 'T-shirt',
-		price: 15
-	};
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
 
-	return new Response(JSON.stringify(data), {
-		status: 200,
-		headers: { 'Content-Type': 'application/json' }
-	});
+export const GET = async () => {
+	try {
+		const products = await prisma.product.findMany();
+
+		return new Response(
+			JSON.stringify({
+				products: products
+			}),
+			{
+				status: 200,
+				headers: {
+					'Content-Type': 'application/json'
+				}
+			}
+		);
+	} catch (error) {
+		return new Response(
+			JSON.stringify({
+				message: 'Something went wrong when trying to get all products.',
+				error: error
+			}),
+			{
+				status: 500,
+				headers: {
+					'Content-Type': 'application/json'
+				}
+			}
+		);
+	}
 };
