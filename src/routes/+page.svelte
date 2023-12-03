140c98331f7f158135e4d23edd2c627f2bd57c60
rkeenan <ross.keenan@protonmail.comm>
2023-12-03 17:22:16 -0600

    Created shopcard component and imported into home page.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,15 @@
 <script>
-	import Button from '../lib/components/Button.svelte';
+	import Button from '$lib/components/Button.svelte';
+	import ShopCard from '$lib/components/ShopCard.svelte';
 	export let data;
 </script>
 
-<div>
-	<p class="text-white">
-		{#if data.products.length > 0}
-			{#each data.products as product}
-				<p>{product.name} / {product.price} / {product.description}</p>
-			{/each}
-		{:else}
-			<p class="w-full text-center pt-32">No products are currently available</p>
-		{/if}
-	</p>
+<div class="grid grid-cols-3 gap-6 mt-6 mb-6">
+	{#if data.products.length > 0}
+		{#each data.products as product}
+			<ShopCard id={product.id} name={product.name} price={product.price} image={product.image} />
+		{/each}
+	{:else}
+		<p class="w-full text-center pt-32">No products are currently available</p>
+	{/if}
 </div>
