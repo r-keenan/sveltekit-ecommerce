e8b788b8350619267b3b3cedb70cf8255726fc78
rkeenan <ross.keenan@protonmail.comm>
2023-12-05 13:38:48 -0600

    added dynamic category pages.

---
+++ src/routes/categories/[slug]/+page.js
@@ -0,0 +1,13 @@
+export async function load({ fetch, params }) {
+	const response = await fetch(`/api/categories/${params.slug}`, {
+		method: 'GET',
+		headers: {
+			'Content-Type': 'application/json',
+			Accept: 'application/json'
+		}
+	});
+
+	const category = await response.json();
+
+	return category;
+}
