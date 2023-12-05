10ffcc5ef404079938c293cc031cbe97674674ce
rkeenan <ross.keenan@protonmail.comm>
2023-12-05 07:46:26 -0600

    Added productcard to categories page.

---
+++ src/routes/categories/+page.js
@@ -0,0 +1,13 @@
+export async function load({ fetch }) {
+	const response = await fetch('/api/products', {
+		method: 'GET',
+		headers: {
+			'Content-Type': 'application/json',
+			Accept: 'application/json'
+		}
+	});
+
+	const products = await response.json();
+
+	return products;
+}
